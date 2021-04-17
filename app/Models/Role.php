38527503d7a2e3173e42a9c79d5d7aca5c1a5e2b
kzubrzycki <kriszub98@gmail.com>
2021-04-17 17:53:41 +0200

    Add Role, User, Seeders

---
+++ app/Models/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use \App\Models\User;
+
+class Role extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name',];
+
+    /**
+     * Get all of the users for the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Role::create(['name' => 'Użytkownik']);
+        \App\Models\Role::create(['name' => 'Administrator']);
+    }
+}
