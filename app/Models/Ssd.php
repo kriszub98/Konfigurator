7f8780d2b36ba01372f4bd8534543b0a082817e4
kzubrzycki <kriszub98@gmail.com>
2021-04-19 14:48:56 +0200

    SSD fix

--- app/Models/Ssd.php
+++ app/Models/Ssd.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sdd extends Model
+class Ssd extends Model
 {
     use HasFactory;
 }
