2372ef1827efda7ac1ecee09b009679e832e380b
PvManiek <maciek.worosz.21@gmial.com>
2021-05-03 14:35:24 +0200

    Create products panel

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::view('/logowanie', 'user.login');        
 Route::view('/rejestracja', 'user.register');    
-Route::view('/uzytkownicy', 'user.index')->name('user.index');        
+Route::view('/uzytkownicy', 'user.index')->name('user.index');     
+Route::view('/produkty', 'user.products')->name('user.products');       
