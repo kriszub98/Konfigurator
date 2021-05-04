cbc81c58c34b7ef3a85e3db244a9ce42a9d592ad
PvManiek <maciek.worosz.21@gmial.com>
2021-05-04 15:12:43 +0200

    Create component view

--- public/js/addprod.js
+++ public/js/addprod.js
@@ -10,7 +10,7 @@ $(function() {
       dynaForm.find('.entry:not(:last) .btn-add')
         .removeClass('btn-add').addClass('btn-remove')
         .removeClass('btn-success').addClass('btn-danger')
-        .html('<span class="glyphicon glyphicon-minus"></span>');
+        .html('<span class="fa fa-minus"></span>');
     }).on('click', '.btn-remove', function(e) {
       $(this).parents('.entry:first').remove();
   
@@ -18,3 +18,34 @@ $(function() {
       return false;
     });
   });
+
+  $(function() {
+    $(document).on('click', '.btn-add2', function(e) {
+      e.preventDefault();
+  
+      var dynaForm = $('.dynamic-wrap2 form:first'),
+        currentEntry = $(this).parents('.entry:first'),
+        newEntry = $(currentEntry.clone()).appendTo(dynaForm);
+  
+      newEntry.find('input').val('');
+      dynaForm.find('.entry:not(:last) .btn-add2')
+        .removeClass('btn-add2').addClass('btn-remove')
+        .removeClass('btn-success').addClass('btn-danger')
+        .html('<span class="fa fa-minus"></span>');
+    }).on('click', '.btn-remove', function(e) {
+      $(this).parents('.entry:first').remove();
+  
+      e.preventDefault();
+      return false;
+    });
+  });
+
+var loadFile = function (event) 
+{
+  var output = document.getElementById('output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () 
+  {
+    URL.revokeObjectURL(output.src) // free memory
+  }
+};
