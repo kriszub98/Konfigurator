347f8cedd9bcbe8541fecdd9b9543ee7767d74e0
kzubrzycki <kriszub98@gmail.com>
2021-06-01 22:32:57 +0200

    Index fix

--- resources/views/sets/index.blade.php
+++ resources/views/sets/index.blade.php
@@ -1,46 +1,56 @@
 @extends('layouts.app')
-@section('title', 'Udostępnione zestawy')
+@section('title', 'Moje zestawy')
 @section('content')
+    <div class="container">
+        @forelse($sets as $set)
+            <a href="{{ route('sets.show', $set) }}">
+                <div class="row card-body border mb-2">
+                    <h2 class="card-title text-black fw-bold text-center">{{ $set->name }}</h2>
 
-<div class="container">
-	<div class="col col-md-12">
-		<div class="card-body border">
-			<div class="row">
-				<div class="col-12">
-				<div class="row">
-					<div class="col">
-                        <h4 class="card-title text-black">Nazwa zestawu</h4>
+                    <div class="col-12 row">
+                        <div class="col-12 col-md-4">
+                            @if ($set->components->contains('is_produced', 0))
+                                <p class="card-text">Możliwy do zrealizowania: Nie</p>
+                            @else
+                                <p class="card-text">Możliwy do zrealizowania: Tak</p>
+                            @endif
+                        </div>
+
+                        @if ($set->is_public == 1)
+                            <div class="col-12 col-md-4">
+                                <p class="card-text">Ocena: {{ $set->ratings->avg('rate') }}/5</p>
+                            </div>
+
+                            <div class="col-12 col-md-4">
+                                <p class="card-text">Liczba ocen: {{ $set->ratings->count() }}</p>
+                            </div>
+                        @else
+                            <p class="col-12 col-md-8">Zestaw prywatny</p>
+                        @endif
+
+                        <div class="col-12 col-md-4">
+                            <div class="card-text">Cena: {{ $set->components->sum('price') }} zł</div>
+                        </div>
+
                     </div>
-					<div class="col d-flex justify-content-end">
-                        <p class="card-text">
-                            <i class="fa fa-user"></i>User
-                        </p>
-					</div>
-				</div>    
-				</div>
-				<div class="col-12">
-					<div class="row">
-						<div class="col md-3 col-12">
-							<p class="card-text">Możliwy do zrealizowania?</p>
-						</div>
-						<div class="col md-3 col-12">
-							<p class="card-text">Prywatny lub ocena(public)</p>
-						</div>
-						<div class="col md-3 col-12">
-							<p  class="card-text">Liczba ocen(public)</p>
-						</div>
-					</div>
-				</div>  
-				<div class="row">
-					<div class="col mt-1">
-						<h5 class="card-title text-danger">Cena?</h5>
-					</div>
-					<div class="col d-flex justify-content-end">
-						<button class="btn-danger">Usuñ</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+
+                    @guest
+                    @else
+                        @if (Auth::user()->role->name == 'Administrator' || $set->user->id == Auth::id())
+                            <div class="row">
+                                <div class="col d-flex justify-content-end">
+                                    <form action="{{ route('sets.destroy', $set) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" type="submit">Usuń</button>
+                                    </form>
+                                </div>
+                            </div>
+                        @endif
+                    @endguest
+                </div>
+            </a>
+        @empty
+        @endforelse
+    </div>
 @endsection
